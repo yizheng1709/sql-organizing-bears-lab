15c041bd7d2f577a57e977efc9d7a2523930a72d
Eva <yi.zheng1709@gmail.com>
2021-02-07 16:53:29 -0500

    commit tree c24695954d47bd7969ca1ebb80d700fa98d5b82e

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    name TEXT,
+    age INTEGER,
+    sex TEXT,
+    color TEXT,
+    temperament TEXT,
+    alive INTEGER,
+    id INTEGER PRIMARY KEY
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES ("Mr. Chocolate", 10, "M", "brown", "chill", 1, 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES ("Rowdy", 11, "M", "brown", "loud", 1, 2);
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES ("Tabitha", 12, "F", "black", "chill", 1, 3);
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES ("Sergeant Brown", 13, "M", "brown", "strict", 1, 4);
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES ("Melissa", 14, "F", "white", "rude", 0, 5);
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES ("Grinch", 15, "M", "black", "lonely", 0, 6);
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES ("Wendy", 16, "F", "red", "rude", 0, 7);
+INSERT INTO bears (name, age, sex, color, temperament, alive, id) VALUES (NULL, 17, "M", "brown", "chill", 1, 8);
+
